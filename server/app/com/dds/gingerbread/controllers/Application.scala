e6a3a54333250fd1d33778bb581ba0c0296b00e0
dchow <dchow@box.com>
2021-03-15 08:25:54 -0700

    initial commit

---
+++ server/app/com/dds/gingerbread/controllers/Application.scala
@@ -0,0 +1,15 @@
+package com.dds.gingerbread.controllers
+
+import javax.inject._
+
+import com.dds.gingerbread.shared.SharedMessages
+import play.api.mvc._
+
+@Singleton
+class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def index = Action {
+    Ok(views.html.index(SharedMessages.itWorks))
+  }
+
+}
---
+++ shared/src/main/scala/com/dds/gingerbread/shared/SharedMessages.scala
@@ -0,0 +1,5 @@
+package com.dds.gingerbread.shared
+
+object SharedMessages {
+  def itWorks = "It works!"
+}
