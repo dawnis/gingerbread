67f0a516bfb771e05e276583c5c733975a151ac7
dchow <dchow@box.com>
2021-03-15 08:34:17 -0700

    seems to work just fine with the ScalaJS tutorial stuff

--- client/src/main/scala/com/dds/gingerbread/ScalaJSExample.scala
+++ client/src/main/scala/com/dds/gingerbread/ScalaJSExample.scala
@@ -2,10 +2,19 @@ package com.dds.gingerbread
 
 import com.dds.gingerbread.shared.SharedMessages
 import org.scalajs.dom
+import org.scalajs.dom.document
 
 object ScalaJSExample {
 
   def main(args: Array[String]): Unit = {
     dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+
+    appendPar(document.body, "Not Hello!")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
   }
 }
