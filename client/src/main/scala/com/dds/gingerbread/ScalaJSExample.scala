fac23a63abd9e71b88b137452fd2a19120c2a253
dchow <dchow@box.com>
2021-03-20 07:57:37 -0700

    scalajs react stuff is finally working!!

--- client/src/main/scala/com/dds/gingerbread/ScalaJSExample.scala
+++ client/src/main/scala/com/dds/gingerbread/ScalaJSExample.scala
@@ -3,37 +3,24 @@ package com.dds.gingerbread
 import com.dds.gingerbread.shared.SharedMessages
 import org.scalajs.dom
 import org.scalajs.dom.document
+import Person.person
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
 
 object ScalaJSExample {
 
-  case class person(name: String)
-  val Trista = person("Trista")
-  val Charlotte = person("Charlotte")
-  val aPerson =  ScalaComponent.builder[person].render_P(person => <.div(f"My name is $person")).build
-
   def main(args: Array[String]): Unit = {
     //dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
-    createHeader(document.body, "My React Learning App")
-    appendPar(document.body, "This is cool!")
+    val rootNode = dom.document.getElementById("root")
+    val trista = person.props(name="Trista", age="39")
+    val charlotte = person.props(name="Charlotte", age="7")
 
     <.div(
-      aPerson(Trista),
-      aPerson(Charlotte)
-    )
-
-  }
-
-  def createHeader(targetNode: dom.Node, text: String): Unit = {
-    val h1Node = document.createElement("h1")
-    targetNode.appendChild(h1Node)
-  }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    parNode.textContent = text
-    targetNode.appendChild(parNode)
+      <.h1("My React Learning App"),
+        <.p("So Cool!"),
+      person.aPerson(trista),
+      person.aPerson(charlotte),
+    ).renderIntoDOM(rootNode)
   }
 }
