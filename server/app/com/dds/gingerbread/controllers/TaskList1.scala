7b05713f5e6c94d11fb459902a0569f30da4c8a8
dchow <dchow@box.com>
2021-03-21 17:36:08 -0700

    Play video series 6 done.

--- server/app/com/dds/gingerbread/controllers/TaskList1.scala
+++ server/app/com/dds/gingerbread/controllers/TaskList1.scala
@@ -10,6 +10,10 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(views.html.login1())
   }
 
+  def validateLoginGet(username: String, password: String) = Action {
+    Ok(s"$username logged in with $password.")
+  }
+
   def taskList = Action {
     val tasks = List("wake", "breakfast", "plan", "work", "play", "repeat")
     Ok(views.html.taskList1(tasks))
