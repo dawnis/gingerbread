48285b12ce65a9e8bfc824cb5d322b4fa78baf18
dchow <dchow@box.com>
2021-03-21 17:51:56 -0700

    Play video 7

--- server/app/com/dds/gingerbread/controllers/TaskList1.scala
+++ server/app/com/dds/gingerbread/controllers/TaskList1.scala
@@ -14,6 +14,17 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(s"$username logged in with $password.")
   }
 
+  //Action is actually a pass by name argument
+  def validateLoginPost = Action { request =>
+    //arguments are not encoded in the url in POST. They are encoded in the body.
+    val postVals: Option[Map[String, Seq[String]]] = request.body.asFormUrlEncoded
+    postVals.map { args =>
+      val username = args("username").head
+      val password = args("password").head
+      Ok(s"$username logged in with password $password")
+    }.getOrElse(Ok("oops"))
+  }
+
   def taskList = Action {
     val tasks = List("wake", "breakfast", "plan", "work", "play", "repeat")
     Ok(views.html.taskList1(tasks))
