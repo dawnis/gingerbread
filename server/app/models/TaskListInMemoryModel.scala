ca4d7be1ed6dc89441e59d9dd0d9b0ab07bc6c8a
dchow <dchow@box.com>
2021-03-23 08:48:17 -0700

    adding some more model methods

--- server/app/models/TaskListInMemoryModel.scala
+++ server/app/models/TaskListInMemoryModel.scala
@@ -5,15 +5,24 @@ import scala.collection.mutable
 object TaskListInMemoryModel {
 
   private val users: mutable.Map[String, String] =  mutable.Map("dchow" -> "mypassword")
+  private val tasks = mutable.Map[String, List[String]]("dchow" -> List("Wake", "Be Awesome", "Sleep"))
 
   def validateUser(username: String, password: String): Boolean  = {
     //returns an option for a username, else false
     users.get(username).map(_ == password).getOrElse(false)
   }
 
-  def createUser(username: String, password: String): Boolean = ???
+  def createUser(username: String, password: String): Boolean = {
+    if (users.contains(username)) false
+    else {
+      users(username) = password
+      true
+    }
+  }
 
-  def getTasks(username: String): Seq[String] = ???
+  def getTasks(username: String): Seq[String] = {
+    tasks.get(username).getOrElse(Nil)
+  }
 
   def addTask(username: String, tasks: String): Unit = ???
 
