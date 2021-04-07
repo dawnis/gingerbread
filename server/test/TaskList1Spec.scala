29f5db63f91ff0a41683e774200449242be39715
dchow <dchow@box.com>
2021-04-07 08:52:33 -0700

    adding some tests, but sti;l with driver problems

--- server/test/TaskList1Spec.scala
+++ server/test/TaskList1Spec.scala
@@ -2,6 +2,7 @@ import org.scalatestplus.play.{HtmlUnitFactory, OneBrowserPerSuite, PlaySpec}
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 
 //port is provided on import
+//this does not play nice with JavaScript
 class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory {
   "Task list 1" must {
     "login and access functions" in {
@@ -12,12 +13,18 @@ class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowser
       find(cssSelector("h2")).foreach(e => e.text mustBe "Login")
       find(cssSelector("h2")).isEmpty mustBe false
       click on "username-login"
-      textField("username").value = "dchow"
+      textField(id("username-login")).value = "dchow"
       click on "password-login"
-      pwdField("password").value = "mypassword"
-
-      eventually {
+      pwdField(id("password-login")).value = "mypassword"
       submit()
+      eventually {
+        println("In Eventually")
+        pageTitle mustBe "Task List"
+        println("Title Passded")
+        find(cssSelector("h2")).isEmpty mustBe false
+        find(cssSelector("h2")).foreach( e => e.text mustBe "Task list")
+        println("h2 found")
+        findAll(cssSelector("li")).toList.map(_.text) mustBe List("Wake", "Sleep", "Be Awesome")
       }
     }
   }
